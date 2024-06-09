5a26c4e0279bd922b14b2896e457e87a5fac7a7c
Oded <oded.frig1@gmail.com>
2024-06-09 19:30:49 +0300

    task1-part1

--- c-sharp-studying-oded-frig/Program.cs
+++ c-sharp-studying-oded-frig/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using c_sharp_studying_oded_frig.task;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace c_sharp_studying_oded_frig
     {
         static void Main(string[] args)
         {
+            Level1.RunMe();
         }
     }
 }
---
+++ c-sharp-studying-oded-frig/task/Level1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_studying_oded_frig.task
+{
+    public class Level1
+    {
+
+        public static void RunMe()
+        {
+            Console.WriteLine("Hi, I am class level 1.");
+            Level2.RunMe();
+
+        }
+    }
+}
